12a91e0eb9af3b3c7e8bd0f0baf73efa72e199d4
Albano Futa <99339143+iNOfutta@users.noreply.github.com>
2024-08-05 22:06:06 +0100

    next.config.js included

---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  images: {
+    domains: [
+      "www.shutterstock.com",
+      "img.huffingtonpost.com",
+      "atlantablackstar.com",
+      "img.einnews.com",
+      "images.ctfassets.net",
+    ],
+  },
+};
